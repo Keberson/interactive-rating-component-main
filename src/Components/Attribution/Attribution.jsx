74e5c0a5bf97d829f99633cfb5a10cf130166e93
Keberson <creedkiller.62@gmail.com>
2022-08-11 14:17:22 +0300

    develop: Fix errors

--- src/Components/Attribution/Attribution.jsx
+++ src/Components/Attribution/Attribution.jsx
@@ -3,8 +3,8 @@ import style from './styles.module.css'
 export const Attribution = ({ styles }) => {
     return (
         <div className={style.attribution} style={styles}>
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-            Coded by <a href="https://github.com/Keberson">Keberson</a>.
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+            Coded by <a href="https://github.com/Keberson" target="_blank" rel="noreferrer">Keberson</a>.
         </div>
     );
 };
